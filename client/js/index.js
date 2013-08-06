4a0f6f3a7214f51d7bb917ab075c536be692f136
Niklas Logren <niklas@niklaslogren.com>
2013-08-06 19:56:05 +0200

    Current track is now populated properly using spotify's api, also on track change.

--- client/js/index.js
+++ client/js/index.js
@@ -1,12 +1,22 @@
-var app = app || {};
+require(['$api/models'], function(models) {
 
-$(function() {
+  $(function() {
 
-  new app.AppView();
+    new app.AppView();
 
-  window.app = app;
-  window.newTrack = function() {
-    app.Tracks.add(new app.Track({artist: "artiste", title: "titell", tags: ["tag1", "tag2", "tag3"]}));
-  }
+    // Add the currently playing track to the Tracks collection
+    var newTrack = function() {
+      var spotifyTrack = models.player.track;
+      app.Tracks.create({artist: spotifyTrack.artists[0].name.decodeForHtml(),
+                         title: spotifyTrack.name.decodeForHtml(),
+                         tags: ["tag1", "tag2", "tag3"]});
+    }
 
+    // Process currently playing track on startup (if any)
+    models.player.load('track').done(function(track) {
+      if (track) newTrack();
+    }); // Listen for future track changes
+    models.player.addEventListener('change', newTrack);
+
+  });
 });
